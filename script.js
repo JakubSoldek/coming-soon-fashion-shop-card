4d3b427117feab679bbef2942e5bf6a69de55ced
Jakub Sołdek <kanonier714@gmail.com>
2021-10-07 14:36:46 +0200

    add js form validation

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputForm = document.querySelector(".input");
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const validateEmail = (email) => emailRegex.test(email);
+
+inputForm.onsubmit = (e) => {
+  e.preventDefault();
+  const userEmail = e.target.email.value;
+  if (userEmail && validateEmail(userEmail)) {
+    e.target.classList.remove("error");
+    console.log(userEmail);
+  } else {
+    // console.log(userEmail);
+    e.target.classList.add("error");
+    
+
+  }
+};
